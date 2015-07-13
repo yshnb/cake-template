e48eacb06fa6b0bff18765099e2e21d8f3757cc5
Tsukasa Hoki <tsukasa.hoki@gmail.com>
2015-07-13 16:03:54 +0900

    changed.

--- app/Controller/FirstController.php
+++ app/Controller/FirstController.php
@@ -3,7 +3,8 @@ App::uses('AppController', 'Controller');
 
 class FirstController extends AppController {
 
-	public $uses = array('First', 'MyRedis');
+	//public $uses = array('First', 'MyRedis');
+	public $uses = array('First');
 
 	public function index() {
         $this->layout = "first_layout";
@@ -11,11 +12,12 @@ class FirstController extends AppController {
 
         $data = $this->First->getAllData();
 
-        $this->MyRedis->set('key', 'value');
-        $this->MyRedis->get('key');
-        $this->MyRedis->incr('pv');
-        $pv = $this->MyRedis->get('pv');
+        //$this->MyRedis->set('key', 'value');
+        //$this->MyRedis->get('key');
+        //$this->MyRedis->incr('pv');
+        //$pv = $this->MyRedis->get('pv');
 
-        $this->set(compact("data", "pv"));
+        //$this->set(compact("data", "pv"));
+        $this->set(compact("data"));
 	}
 }
