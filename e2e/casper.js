d968326e672d1a5255b133d9812e2fe179804e38
Tsukasa Hoki <tsukasa.hoki@gmail.com>
2015-07-15 13:57:11 +0900

    changed.

--- e2e/casper.js
+++ e2e/casper.js
@@ -1,5 +1,6 @@
 casper.test.begin('First Test Running...', 3, function suite(test) {
     casper.start("http://127.0.0.1:8080/", function() {
+        this.capture('image.png');
         test.assertExists("h1");
         test.assertSelectorHasText("h1", 'Title');
         test.assertVisible('div.description');
